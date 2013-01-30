7b08dd66b56ccdd3e453467a3e70cdfe58ae8b77
Yu-Shan Sun <yushans@g.clemson.edu>
2013-01-30 13:40:28 -0500

    added performance files

--- RESOLVE/Main/Concepts/Stack_Template/SSC - Original.pp
+++ RESOLVE/Main/Concepts/Stack_Template/SSC - Original.pp
@@ -1,5 +1,5 @@
 Profile SSC short_for Space_Conscious for Stack_Template;
-	uses Real_Number_Theory;--, Duration_Basics_Theory;
+	uses Real_Theory;
 
 	Definition SSCI1: R = Real_0; 
 	Definition SSCI2: R = Real_0; 
@@ -12,28 +12,23 @@ Profile SSC short_for Space_Conscious for Stack_Template;
 	Definition SSCC1: R = Real_0; 
 	Definition SSCC2: R = Real_0; 
 
-	Definition I_Dur(e : Entity) : R;
-	Definition F_Dur(e : Entity) : R;
-	Definition F_IV_Dur(e : Entity) : R;
-	Definition Cnts_Dur(s : Str(Entry)) : R; 
-	Definition (i: Z) - (j: N): Z = 0;
+
+	Definition Cnts_Dur: Str( Entry) = empty_string; 
 
     Type Family Stack is modeled by Str( Entry );
 
 	initialization
-		duration  SSCI1 + (SSCI2 + I_Dur(Entry)) * Max_Depth;
+		duration  SSCI1 + (SSCI2 + Entry.I_Dur ) * Max_Depth;
 
         finalization 
-	 duration  SSCF1 + Cnts_Dur( #S) + ( SSCF2 + F_IV_Dur(Entry) ) * ( Max_Depth - |#S| );
+		duration  SSCF1 + Cnts_Dur( #S) + ( SSCF2 + Entry.F_IV_Dur ) * ( Max_Depth -|#S| );
 
-   --   Oper Push( clears E: Entry; updates S: Stack );
-      Oper Push( alters E: Entry; updates S: Stack );
+      Oper Push( clears E: Entry; updates S: Stack );
 	duration  SSCPu;
 
       Oper Pop( replaces R: Entry; updates S: Stack );
-	ensures  Entry.Is_Initial(R);	
-	  duration  ( SSCPo + I_Dur(Entry) ) + F_Dur (#R);
---	  duration  ( SSCPo + Entry.I_Dur ) + F_Dur (#R);
+	ensures  Entry.Is_init (R);	
+	  duration  ( SSCPo + Entry.I_Dur ) + Entry.F_Dur( #R );
 
 
       Oper Depth( restores S: Stack ): Integer;
@@ -43,7 +38,7 @@ Profile SSC short_for Space_Conscious for Stack_Template;
         duration  SSCRc;
 
       Oper Clear( clears S: Stack );	
-	duration  SSCC1 + Cnts_Dur( #S ) + ( SSCC2 + F_IV_Dur(Entry) ) * ( Max_Depth -|#S| );
+	duration  SSCC1 + Cnts_Dur( #S) + ( SSCC2 + Entry.F_IV_Dur )*( Max_Depth -|#S| );
 
 end SSC;
 
