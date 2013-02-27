d6eee4188aff875057c0b03f94831689b08b46f1
Nighat <yasmin@clemson.edu>
2013-02-27 12:15:55 -0500

    put the right duration for Do_Nothing

--- RESOLVE/Main/Concepts/Stack_Template/DNSSC.pp
+++ RESOLVE/Main/Concepts/Stack_Template/DNSSC.pp
@@ -1,10 +1,12 @@
 Profile DNSSC short_for Do_Nothing_Space_Conscious for Do_Nothing_Capability for Stack_Template with_profile SSC;
 	uses Integer_Theory;
 	
+	Definition 2: Z;
+
 	Definition SSCDN: R = SSCPo + SSCPu;
 
 	Operation Do_Nothing(restores S: Stack);
-		--duration  SSCDN + 2*I_Dur(Entry) + 2*F_IV_Dur(Entry);
-		duration SSCDN;
+		duration  SSCDN + 2*I_Dur(Entry) + 2*F_IV_Dur(Entry);
+		--duration SSCDN;
 
 end DNSSC;
